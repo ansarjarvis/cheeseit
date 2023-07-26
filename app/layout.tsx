0448675f4bec7fb1800fc9c86cddcb04e49665e8
Mohammad Ansar <ansar.ofcl00@gmail.com>
2023-07-27 03:37:45 +0530

    feat:initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "cheeseit",
+  description: "A Reddit Clone built with Next js and TypeScript",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <div className="container max-w-7xl h-full pt-12">{children}</div>
+      </body>
+    </html>
+  );
+}
