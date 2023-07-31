db966dc91220e065650b342a4cab995da33ee045
Mohammad Ansar <ansar.ofcl00@gmail.com>
2023-08-01 03:10:42 +0530

    feat: add react query provider for query and mutation

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/Toaster";
+import Provider from "@/components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,12 +29,14 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        <Navbar />
+        <Provider>
+          <Navbar />
 
-        {authModal}
+          {authModal}
 
-        <div className="container max-w-7xl h-full pt-12">{children}</div>
-        <Toaster />
+          <div className="container max-w-7xl h-full pt-12">{children}</div>
+          <Toaster />
+        </Provider>
       </body>
     </html>
   );
